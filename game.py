3f7370c14ada643aed4ff17824ec2a6cf9fcac27
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-20 15:55:56 -0400

    print the board

--- game.py
+++ game.py
@@ -3,3 +3,7 @@ class TicTacToe:
         # we will use a single list to rep 3x3 board
         self.board = [' ' for _ in range(9)]
         self.current_winner = None  # keep track of winner!
+
+    def print_board(self):
+        for row in [self.board[i * 3: (i + 1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' | ')
