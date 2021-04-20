57ba58227a7940f685b10cc8254d6d074ef5eb25
Kiara Guzman <kiara.kg11@gmail.com>
2021-04-20 16:06:56 -0400

    available moves defined

--- game.py
+++ game.py
@@ -5,5 +5,24 @@ class TicTacToe:
         self.current_winner = None  # keep track of winner!
 
     def print_board(self):
+        # this is just for getting the rows
         for row in [self.board[i * 3: (i + 1) * 3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' | ')
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tells us what number corresponds to what box)
+        number_board = [[(str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        # cleaner way to write code below
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+
+        # moves = []
+        # for (i, spot) in enumerate(self.board):
+        #     # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
+        #     if spot ==  ' ':
+        #         moves.append(i)
+        # returns moves
